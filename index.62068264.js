03d9533d6cf8b3cdb70a97a20c3221c2e3463f6c
Yuliia21BM <108929319+Yuliia21BM@users.noreply.github.com>
2022-10-12 22:23:54 +0000

    Deploying to gh-pages from @ Yuliia21BM/coffee-time-proj@b5f9caa8171f38f18751ad0103c2c131caa11541 🚀

---
+++ index.62068264.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",spaceBetween:100,loop:!0,speed:2500,autoplay:{delay:7e3},pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.62068264.js.map
